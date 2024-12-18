e8a787b8bbc1f78ca532585236383b5815978642
Atul Vishwakarma <atulvishw240@gmail.com>
2024-12-18 13:16:49 +0530

    Let node track where it belongs

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,12 @@
 # Board Vertex OR Square
 class Node
   attr_accessor :links, :parent
+  attr_reader :x, :y
 
-  def initialize
+  def initialize(x, y)
     @links = []
     @parent = nil
+    @x_coordinate = x
+    @y_coordinate = y
   end
 end
