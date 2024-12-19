a0c00824db2f3bc1b0804c0857432a4701a3e6f1
Atul Vishwakarma <atulvishw240@gmail.com>
2024-12-19 08:17:51 +0530

    Delete knight.rb

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,7 @@
 # Board Vertex OR Square
 class Node
-  attr_accessor :links, :parent
-  attr_reader :x_coordinate, :y_coordinate, :piece
+  attr_accessor :links, :parent, :piece
+  attr_reader :x_coordinate, :y_coordinate
 
   def initialize(x, y, piece = nil)
     @links = []
